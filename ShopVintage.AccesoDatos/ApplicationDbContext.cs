8cefa2622e8cb1b9f7d76232aef7add2ff5f9a94
Ezequiel Di Giacomo <ezequiel250891@hotmail.com>
2023-08-20 14:43:35 -0300

    Creacion de entidades

--- ShopVintage.AccesoDatos/ApplicationDbContext.cs
+++ ShopVintage.AccesoDatos/ApplicationDbContext.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using VintageShop.Entidades;
 
 namespace ShopVintage.AccesoDatos
 {
@@ -17,143 +18,150 @@ namespace ShopVintage.AccesoDatos
 
         }
 
-        //public DbSet<Usuario> Usuarios { get; set; }
-        //public DbSet<Zapato> Zapatos { get; set; }
-        //public DbSet<Talle> Talles { get; set; }
-        //public DbSet<Pago> Pagos { get; set; }
-        //public DbSet<FormaDePago> FormaDePagos { get; set; }
-        //public DbSet<Marca> Marcas { get; set; }
-        //public DbSet<Stock> Stocks { get; set; }
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Zapato> Zapatos { get; set; }
+        public DbSet<Talle> Talles { get; set; }
+        public DbSet<Pago> Pagos { get; set; }
+        public DbSet<FormaDePago> FormaDePagos { get; set; }
+        public DbSet<Marca> Marcas { get; set; }
+        public DbSet<Stock> Stocks { get; set; }
 
 
-        //protected override void OnModelCreating(ModelBuilder modelBuilder)
-        //{
-        //    base.OnModelCreating(modelBuilder);
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
 
 
-        //    //--------Usuarios--------
-        //    modelBuilder.Entity<Usuario>()
-        //        .HasKey(x => x.Id);
+            //--------Usuarios--------
+            modelBuilder.Entity<Usuario>()
+                .HasKey(x => x.Id);
 
-        //    modelBuilder.Entity<Usuario>()
-        //        .Property(x => x.Nombre)
-        //        .IsRequired();
+            modelBuilder.Entity<Usuario>()
+                .Property(x => x.Nombre)
+                .IsRequired();
 
-        //    modelBuilder.Entity<Usuario>()
-        //        .Property(x => x.Apellido)
-        //        .IsRequired();
+            modelBuilder.Entity<Usuario>()
+                .Property(x => x.Apellido)
+                .IsRequired();
 
-        //    modelBuilder.Entity<Usuario>()
-        //       .Property(x => x.Email)
-        //       .IsRequired();
-
-
-        //    //--------Zapato--------
-
-        //    modelBuilder.Entity<Zapato>()
-        //        .HasKey(x => x.Id);
-
-        //    modelBuilder.Entity<Zapato>()
-        //       .Property(x => x.Modelo)
-        //       .IsRequired();
-
-        //    modelBuilder.Entity<Zapato>()
-        //       .Property(x => x.Precio)
-        //       .IsRequired();
-
-        //    modelBuilder.Entity<Zapato>()
-        //       .Property(x => x.Imagen);
-
-        //    //modelBuilder.Entity<Zapato>()
-        //    //            .HasOne(x => x.Marca)
-        //    //            .WithMany(m => m.ZapatosList)
-        //    //            .HasForeignKey(x => x.IdMarca);
-
-        //    //modelBuilder.Entity<Zapato>()
-        //    //            .HasOne(x => x.TallesList)
-        //    //            .WithMany(m => m)
+            modelBuilder.Entity<Usuario>()
+               .Property(x => x.Email)
+               .IsRequired();
 
 
+            //--------Zapato--------
 
-        //    //--------Talle--------
+            modelBuilder.Entity<Zapato>()
+                .HasKey(x => x.Id);
 
-        //    modelBuilder.Entity<Talle>()
-        //        .HasKey(x => x.Id);
+            modelBuilder.Entity<Zapato>()
+               .Property(x => x.IdMarca);
 
-        //    modelBuilder.Entity<Talle>()
-        //        .Property(x => x.TalleNumero);
+            modelBuilder.Entity<Zapato>()
+               .Property(x => x.Modelo)
+               .IsRequired();
 
-        //    modelBuilder.Entity<Talle>()
-        //        .Property(x => x.IdZapato);
+            modelBuilder.Entity<Zapato>()
+               .Property(x => x.Precio)
+               .IsRequired();
 
-        //    //modelBuilder.Entity<Talle>()
-        //    //            .HasOne(x => x.Zapato)
-        //    //            .WithMany(m => m.TallesList)
-        //    //            .HasForeignKey(x => x.IdZapato);
+            modelBuilder.Entity<Zapato>()
+               .Property(x => x.Imagen);
 
+            //modelBuilder.Entity<Zapato>()
+            //            .HasOne(x => x.Marca)
+            //            .WithMany(m => m.ZapatosList)
+            //            .HasForeignKey(x => x.IdMarca);
 
-        //    //--------Pago--------
-
-        //    modelBuilder.Entity<Pago>()
-        //                .HasKey(x => x.Id);
-
-        //    modelBuilder.Entity<Pago>()
-        //                .Property(x => x.IdFormaDePago)
-        //                .IsRequired();
-
-        //    //modelBuilder.Entity<Pago>()
-        //    //            .HasOne(fp => fp.FormaDePagoUtilizada)
-        //    //            .WithMany(fp => fp.Pagos)
-        //    //            .HasForeignKey(x => x.IdFormaDePago);
-
-        //    //modelBuilder.Entity<Pago>()
-        //    //            .HasOne(z => z.ZapatoSeleccionado)
-        //    //            .WithMany(p => p.PagoUtilizado)
-        //    //            .HasForeignKey(x => x.IdPrecioZapato);
+            //modelBuilder.Entity<Zapato>()
+            //            .HasOne(x => x.TallesList)
+            //            .WithMany(m => m)
 
 
 
-        //    //--------FormaDePago--------
+            //--------Talle--------
 
-        //    modelBuilder.Entity<FormaDePago>()
-        //        .HasKey(x => x.Id);
+            modelBuilder.Entity<Talle>()
+                .HasKey(x => x.Id);
 
-        //    modelBuilder.Entity<FormaDePago>()
-        //        .Property(x => x.Descripcion)
-        //        .IsRequired();
+            modelBuilder.Entity<Talle>()
+                .Property(x => x.IdZapato);
 
-        //    //--------Marca--------
+            modelBuilder.Entity<Talle>()
+                .Property(x => x.TalleNumero);
 
-        //    modelBuilder.Entity<Marca>()
-        //        .HasKey(x => x.Id);
 
-        //    modelBuilder.Entity<Marca>()
-        //        .Property(x => x.Nombre)
-        //        .IsRequired();
+            //modelBuilder.Entity<Talle>()
+            //            .HasOne(x => x.Zapato)
+            //            .WithMany(m => m.TallesList)
+            //            .HasForeignKey(x => x.IdZapato);
 
-        //    //--------Stock--------
 
-        //    modelBuilder.Entity<Stock>()
-        //        .HasKey(x => x.Id);
+            //--------Pago--------
 
-        //    modelBuilder.Entity<Stock>()
-        //        .Property(x => x.IdZapato)
-        //        .IsRequired();
+            modelBuilder.Entity<Pago>()
+                        .HasKey(x => x.Id);
 
-        //    modelBuilder.Entity<Stock>()
-        //        .Property(x => x.IdTalle)
-        //        .IsRequired();
+            modelBuilder.Entity<Pago>()
+                        .Property(x => x.IdFormaDePago);
 
-        //    modelBuilder.Entity<Stock>()
-        //        .Property(x => x.Cantidad)
-        //        .IsRequired();
+            modelBuilder.Entity<Pago>()
+                        .Property(x => x.IdPrecioZapato);
 
-        //    //modelBuilder.Entity<Stock>()
-        //    //            .HasOne(z => z.Zapato)
-        //    //            .WithMany(s => s.StocksList)
-        //    //            .HasForeignKey(t =>t.IdTalle);
 
-       //}
+            //modelBuilder.Entity<Pago>()
+            //            .HasOne(fp => fp.FormaDePagoUtilizada)
+            //            .WithMany(fp => fp.Pagos)
+            //            .HasForeignKey(x => x.IdFormaDePago);
+
+            //modelBuilder.Entity<Pago>()
+            //            .HasOne(z => z.ZapatoSeleccionado)
+            //            .WithMany(p => p.PagoUtilizado)
+            //            .HasForeignKey(x => x.IdPrecioZapato);
+
+
+
+            //--------FormaDePago--------
+
+            modelBuilder.Entity<FormaDePago>()
+                .HasKey(x => x.Id);
+
+            modelBuilder.Entity<FormaDePago>()
+                .Property(x => x.Descripcion)
+                .IsRequired();
+
+            //--------Marca--------
+
+            modelBuilder.Entity<Marca>()
+                .HasKey(x => x.Id);
+
+            modelBuilder.Entity<Marca>()
+                .Property(x => x.Nombre)
+                .IsRequired();
+
+            //--------Stock--------
+
+            modelBuilder.Entity<Stock>()
+                .HasKey(x => x.Id);
+
+            modelBuilder.Entity<Stock>()
+                .Property(x => x.IdZapato)
+                .IsRequired();
+
+            modelBuilder.Entity<Stock>()
+                .Property(x => x.IdTalle)
+                .IsRequired();
+
+            modelBuilder.Entity<Stock>()
+                .Property(x => x.Cantidad)
+                .IsRequired();
+
+            //modelBuilder.Entity<Stock>()
+            //            .HasOne(z => z.Zapato)
+            //            .WithMany(s => s.StocksList)
+            //            .HasForeignKey(t =>t.IdTalle);
+
+        }
     }
 }
     
---
+++ ShopVintage.AccesoDatos/Migrations/20230820174040_CreacionTablas.cs
@@ -0,0 +1,139 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ShopVintage.AccesoDatos.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreacionTablas : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "FormaDePagos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Descripcion = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FormaDePagos", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Marcas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Marcas", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Pagos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    IdFormaDePago = table.Column<int>(type: "int", nullable: false),
+                    IdPrecioZapato = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pagos", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Stocks",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    IdZapato = table.Column<int>(type: "int", nullable: false),
+                    IdTalle = table.Column<int>(type: "int", nullable: false),
+                    Cantidad = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Stocks", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Talles",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    IdZapato = table.Column<int>(type: "int", nullable: false),
+                    TalleNumero = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Talles", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Usuarios",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Apellido = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuarios", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Zapatos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    IdMarca = table.Column<int>(type: "int", nullable: false),
+                    Modelo = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Precio = table.Column<double>(type: "float", nullable: false),
+                    Imagen = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Zapatos", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "FormaDePagos");
+
+            migrationBuilder.DropTable(
+                name: "Marcas");
+
+            migrationBuilder.DropTable(
+                name: "Pagos");
+
+            migrationBuilder.DropTable(
+                name: "Stocks");
+
+            migrationBuilder.DropTable(
+                name: "Talles");
+
+            migrationBuilder.DropTable(
+                name: "Usuarios");
+
+            migrationBuilder.DropTable(
+                name: "Zapatos");
+        }
+    }
+}
